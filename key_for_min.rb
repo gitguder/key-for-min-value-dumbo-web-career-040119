4b0ad5976ceeb5aeed48bc533da5afd8d12e701f
Codey Pinero <codey.pinero@gmail.com>
2019-03-27 00:00:01 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,5 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
-{ :dogs => 3, :cats: 4, :lizards => 7}
 
 def key_for_min_value(name_hash)
   name_hash.each do |key, val|
