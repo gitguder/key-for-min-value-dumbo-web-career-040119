4798597e7409d612def034f712dd84a01703cd6a
Codey Pinero <codey.pinero@gmail.com>
2019-03-27 00:08:03 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,6 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  lowest_val =
   name_hash.each do |key, val|
     if name_hash[key] < val
       name_hash[val]
