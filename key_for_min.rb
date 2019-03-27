edcef5ea52bc1eb31bef18413f5d5be26e7bac3a
Codey Pinero <codey.pinero@gmail.com>
2019-03-27 00:08:24 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  lowest_val =
+  lowest_val = nil
   name_hash.each do |key, val|
     if name_hash[key] < val
       name_hash[val]
